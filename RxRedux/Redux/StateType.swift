de8ac4a499099c0bd41b48a1ca1f24ccab5b9e50
ChrisAU <cjnevin@gmail.com>
2018-03-02 17:24:29 +0000

    Reduce in background.

--- RxRedux/Redux/StateType.swift
+++ RxRedux/Redux/StateType.swift
@@ -5,7 +5,7 @@ protocol StateType {
 }
 
 extension StateType {
-    private func reduced(_ action: ActionType) -> Self {
+    func reduced(_ action: ActionType) -> Self {
         var newState = self
         newState.reduce(action)
         return newState
@@ -14,17 +14,16 @@ extension StateType {
     func loop(on actionStream: Observable<ActionType>,
               with sideEffects: [SideEffect<Self>]) -> Observable<Self> {
         return actionStream
-            .observeOn(MainScheduler.instance)
+            .observeOn(SerialDispatchQueueScheduler(qos: .userInitiated))
             .startWith(Init())
             .scan((state: self, applied: nil), accumulator: { (state, action) in
                 (state.state.reduced(action), action)
             })
-            .observeOn(SerialDispatchQueueScheduler(qos: .userInitiated))
             .do(onNext: { (state, action) in
                 sideEffects.forEach { $0(state, action!) }
             })
-            .observeOn(MainScheduler.instance)
             .map({ $0.state })
+            .observeOn(MainScheduler.instance)
             .share(replay: 1, scope: SubjectLifetimeScope.whileConnected)
     }
 }
